94c12b366c172a19afa0030ff0981bce3058fa9b
vathdev <vthien562004@gmail.com>
2025-10-03 02:51:31 +0700

    Edit cor socket

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: "http://34.234.93.66:3000",
 		methods: ["GET", "POST"],
 	},
 });
